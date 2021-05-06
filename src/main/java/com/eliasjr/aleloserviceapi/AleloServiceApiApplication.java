a896a5fee3c8f777e623bab9e4b6daf1c7003983
eliasjr <a49888ce3c@emailmonkey.club>
2021-05-05 20:43:01 -0400

    create project

---
+++ src/main/java/com/eliasjr/aleloserviceapi/AleloServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.eliasjr.aleloserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleloServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AleloServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliasjr/aleloserviceapi/AleloServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliasjr.aleloserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleloServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
